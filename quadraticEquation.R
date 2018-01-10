23f3afc817f993eba0bfd33dd4785b80e553b98d
Sergio Giraldo <sgiraldo@uolinc.com>
2018-01-10 09:05:14 -0200

    quadratic

---
+++ quadraticEquation.R
@@ -0,0 +1,23 @@
+# Constructing Quadratic Formula (only for real solutions)
+quadratic <- function(a,b,c){
+  if(delta(a,b,c) > 0){ # first case D>0
+    x_1 = (-b+sqrt(delta(a,b,c)))/(2*a)
+    x_2 = (-b-sqrt(delta(a,b,c)))/(2*a)
+    result = c(x_1,x_2)
+  }
+  else if(delta(a,b,c) == 0){ # second case D=0
+    x = -b/(2*a)
+  }
+  else {"There are no real roots."} # third case D<0
+}
+
+# Constructing delta
+delta<-function(a,b,c){
+  b^2-4*a*c
+}
+
+result <- quadratic(1,-5,6)
+result
+
+---
+  
