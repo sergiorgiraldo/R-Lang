f7fe71959b8285b0e34c8dc8cf353fe1434fa62a
sergio giraldo <sergiorgiraldo@gmail.com>
2018-01-29 22:06:04 -0200

    updates

---
+++ geneticAlgorithm101.R
@@ -0,0 +1,30 @@
+library(genalg)
+
+dataset <- data.frame(item = c("pocketknife", "beans", "potatoes", "onions", 
+                               "sleeping bag", "rope", "compass"), 
+                      survivalpoints = c(10, 20, 15, 2, 
+                                         30,10, 30), 
+                      weight = c(1, 5, 10, 1, 
+                                 7, 5, 1))
+weightlimit <- 20
+chromosome = c(1, 0, 0, 1, 1, 0, 0)
+dataset[chromosome == 1, ]
+cat(chromosome %*% dataset$survivalpoints)
+evalFunc <- function(x) {
+  current_solution_survivalpoints <- x %*% dataset$survivalpoints
+  current_solution_weight <- x %*% dataset$weight
+  
+  if (current_solution_weight > weightlimit) 
+    return(0) else return(-current_solution_survivalpoints)
+}
+
+iter = 100
+GAmodel <- rbga.bin(size = 7, popSize = 200, iters = iter, mutationChance = 0.01, 
+                    elitism = T, evalFunc = evalFunc)
+cat(summary(GAmodel))
+solution<-GAmodel$population[which.min(GAmodel$evaluations),]
+dataset[solution == 1, ]
+cat(solution %*% dataset$survivalpoints)
+cat(solution %*% dataset$weight)
+
+
