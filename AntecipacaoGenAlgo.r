3742bcf4f946a2a11310eac3a83940154b1fa7cd
Sérgio Giraldo <sergiorgiraldo@gmail.com>
2018-02-27 13:14:54 -0300

    fixes

--- AntecipacaoGenAlgo.r
+++ AntecipacaoGenAlgo.r
@@ -1,16 +1,30 @@
+
 library("genalg")
-#get transactions from db
-values <- c(10,12,20,30,40,12,3,12,15,15,30,15,41,10,12) 
-howMuchToAntecipate <- 82
-evalFunc <- function(chromosome) {
-  currentSum <- sum(chromosome %*% values)
-  if (currentSum > howMuchToAntecipate ) 
-    return(sum(values) + 1) else return(-currentSum)
+
+transactions <- sample.int(1000, sample.int(300, 1))
+
+howMuchToAntecipate <- 3000
+
+evalFunc <- function(chromossome) {
+  currentSum <- sum(chromossome %*% transactions)
+  if (currentSum > howMuchToAntecipate )
+    return(sum(transactions) + 1)
+  else
+    return(-currentSum)
 }
-iter = 50
-GAmodel <- rbga.bin(size = length(values), popSize = 200, iters = iter, mutationChance = 0.01, 
-                    elitism = T, evalFunc = evalFunc)
+
+numberOfIterations = 100
+sizeOfPopulation = 100
+GAmodel <- rbga.bin(size = length(transactions),
+                    popSize = sizeOfPopulation,
+                    iters = numberOfIterations,
+                    mutationChance = 0.01,
+                    elitism = T,
+                    evalFunc = evalFunc)
 solution<-GAmodel$population[which.min(GAmodel$evaluations),]
-values[solution == 1]
-sum(values[solution == 1])
-#plot(GAmodel)
+
+transactions
+transactions[solution == 1]
+sum(transactions[solution == 1])
+
+plot(GAmodel)
