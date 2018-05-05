70bb3d1c4d3a726ad0c5e90f0b971b3506081b88
sergio giraldo <sergiorgiraldo@gmail.com>
2018-05-04 23:02:15 -0300

    20180504

--- juan/ga.R
+++ juan/ga.R
@@ -1,58 +1,63 @@
 library(genalg)
+
 teams <- read.csv("C:\\Users\\sgiraldo\\source\\python\\juan\\teams.csv",sep=";")
 floorCapacity <- 10
 
 #********************************************************#
 #Fitness function
-evalFunc <- function(x) {
+evalFunc <- function(chromosome) {
   
-  mondayTeams    <- teams[x[(nrow(teams) * 0 + 1) :(1 *nrow(teams))] == 1, ]
-  tuesdayTeams   <- teams[x[(nrow(teams) * 1 + 1) :(2 *nrow(teams))] == 1, ]
-  wednesdayTeams <- teams[x[(nrow(teams) * 2 + 1) :(3 *nrow(teams))] == 1, ]
-  thursdayTeams  <- teams[x[(nrow(teams) * 3 + 1) :(4 *nrow(teams))] == 1, ]
-  fridayTeams    <- teams[x[(nrow(teams) * 4 + 1) :(5 *nrow(teams))] == 1, ]
+  #aux variables
+  mondayTeams    <- teams[chromosome[(nrow(teams) * 0 + 1) : (1 * nrow(teams))] == 1, ]
+  tuesdayTeams   <- teams[chromosome[(nrow(teams) * 1 + 1) : (2 * nrow(teams))] == 1, ]
+  wednesdayTeams <- teams[chromosome[(nrow(teams) * 2 + 1) : (3 * nrow(teams))] == 1, ]
+  thursdayTeams  <- teams[chromosome[(nrow(teams) * 3 + 1) : (4 * nrow(teams))] == 1, ]
+  fridayTeams    <- teams[chromosome[(nrow(teams) * 4 + 1) : (5 * nrow(teams))] == 1, ]
   
-  mondayValues    <- x[(nrow(teams) * 0 + 1) :(1 *nrow(teams))]
-  tuesdayValues   <- x[(nrow(teams) * 1 + 1) :(2 *nrow(teams))]
-  wednesdayValues <- x[(nrow(teams) * 2 + 1) :(3 *nrow(teams))]
-  thursdayValues  <- x[(nrow(teams) * 3 + 1) :(4 *nrow(teams))]
-  fridayValues    <- x[(nrow(teams) * 4 + 1) :(5 *nrow(teams))]
+  mondayValues    <- chromosome[(nrow(teams) * 0 + 1) : (1 * nrow(teams))]
+  tuesdayValues   <- chromosome[(nrow(teams) * 1 + 1) : (2 * nrow(teams))]
+  wednesdayValues <- chromosome[(nrow(teams) * 2 + 1) : (3 * nrow(teams))]
+  thursdayValues  <- chromosome[(nrow(teams) * 3 + 1) : (4 * nrow(teams))]
+  fridayValues    <- chromosome[(nrow(teams) * 4 + 1) : (5 * nrow(teams))]
   
   #********************************************************#
   #restriction 1: available seats
-  curr_capacity_monday <- (floorCapacity - sum(mondayTeams$total))
-  ifelse(curr_capacity_monday < 0, curr_capacity_monday <- 999, curr_capacity_monday <- curr_capacity_monday/floorCapacity)
+  currCapacityMonday	 <- (floorCapacity - sum(mondayTeams$total))
+  ifelse(currCapacityMonday	 < 0, currCapacityMonday <- 999, currCapacityMonday	 <- currCapacityMonday/floorCapacity)
   
-  curr_capacity_tuesday <- (floorCapacity - sum(tuesdayTeams$total))
-  ifelse(curr_capacity_tuesday < 0, curr_capacity_tuesday <- 999, curr_capacity_tuesday <- curr_capacity_tuesday/floorCapacity)
+  currCapacityTuesday <- (floorCapacity - sum(tuesdayTeams$total))
+  ifelse(currCapacityTuesday < 0, currCapacityTuesday <- 999, currCapacityTuesday <- currCapacityTuesday/floorCapacity)
   
-  curr_capacity_wednesday <- (floorCapacity - sum(wednesdayTeams$total))
-  ifelse(curr_capacity_wednesday < 0, curr_capacity_wednesday <- 999, curr_capacity_wednesday <- curr_capacity_wednesday/floorCapacity)
+  currCapacityWednesday <- (floorCapacity - sum(wednesdayTeams$total))
+  ifelse(currCapacityWednesday < 0, currCapacityWednesday <- 999, currCapacityWednesday <- currCapacityWednesday/floorCapacity)
   
-  curr_capacity_thursday <- (floorCapacity - sum(thursdayTeams$total))
-  ifelse(curr_capacity_thursday < 0, curr_capacity_thursday <- 999, curr_capacity_thursday <- curr_capacity_thursday/floorCapacity)
+  currCapacityThursday <- (floorCapacity - sum(thursdayTeams$total))
+  ifelse(currCapacityThursday < 0, currCapacityThursday <- 999, currCapacityThursday <- currCapacityThursday/floorCapacity)
   
-  curr_capacity_friday <- (floorCapacity - sum(fridayTeams$total))
-  ifelse(curr_capacity_friday < 0, curr_capacity_friday <- 999, curr_capacity_friday <- curr_capacity_friday/floorCapacity)
+  currCapacityFriday <- (floorCapacity - sum(fridayTeams$total))
+  ifelse(currCapacityFriday < 0, currCapacityFriday <- 999, currCapacityFriday <- currCapacityFriday/floorCapacity)
   
   #********************************************************#
   #restriction 2: every team must not appear all days in a row. 
-  curr_violations_consecutiveDays_all <- 0 
-  curr_violations_consecutiveDays_none <- 0
+  currViolationsConsecutiveDaysAll <- 0 
+  currViolationsConsecutiveDaysNone <- 0
   for (idx in 1: nrow(teams)){
     #teams at ps all days
     if (sum(mondayValues[idx], tuesdayValues[idx], wednesdayValues[idx], thursdayValues[idx], fridayValues[idx]) == 5){
-      curr_violations_consecutiveDays_all <- 999
+      currViolationsConsecutiveDaysAll <- 999
+      break
     }
+
     #teams at home all days
     if (sum(mondayValues[idx], tuesdayValues[idx], wednesdayValues[idx], thursdayValues[idx], fridayValues[idx]) == 0){
-      curr_violations_consecutiveDays_none <- 999
+      currViolationsConsecutiveDaysNone <- 999
+      break
     }
   }
   
-  #if this restriction must not be met
-  #curr_violations_consecutiveDays_all <- curr_violations_consecutiveDays_all / nrow(teams) #normalization
-  #curr_violations_consecutiveDays_none <- curr_violations_consecutiveDays_none / nrow(teams) #normalization
+  #if this restriction is not a blocker
+  #currViolationsConsecutiveDaysAll <- currViolationsConsecutiveDaysAll / nrow(teams) #normalization
+  #currViolationsConsecutiveDaysNone <- currViolationsConsecutiveDaysNone / nrow(teams) #normalization
   
   #********************************************************#
   #restriction 3: dependencies must be enforced
@@ -71,33 +76,32 @@ evalFunc <- function(x) {
     return(violations)
   }
 
-  totalDependencies <- 0
-  
+  totalDependencies <- 0 
   for (dependencies in teams$dependsOn){
     if (dependencies != ""){
       totalDependencies <- totalDependencies + length(unlist(strsplit(dependencies,",")))
     }
   }  
 
-  curr_violations_dependencies_monday     <- checkDependencies(mondayTeams)/totalDependencies #normalization
-  curr_violations_dependencies_tuesday    <- checkDependencies(tuesdayTeams)/totalDependencies #normalization
-  curr_violations_dependencies_wednesday  <- checkDependencies(wednesdayTeams)/totalDependencies #normalization
-  curr_violations_dependencies_thursday   <- checkDependencies(thursdayTeams)/totalDependencies #normalization
-  curr_violations_dependencies_friday     <- checkDependencies(fridayTeams)/totalDependencies #normalization
+  currViolationsDependenciesMonday	   <- checkDependencies(mondayTeams)/totalDependencies #normalization
+  currViolationsDependenciesTuesday    <- checkDependencies(tuesdayTeams)/totalDependencies #normalization
+  currViolationsDependenciesWednesday  <- checkDependencies(wednesdayTeams)/totalDependencies #normalization
+  currViolationsDependenciesThursday   <- checkDependencies(thursdayTeams)/totalDependencies #normalization
+  currViolationsDependenciesFriday     <- checkDependencies(fridayTeams)/totalDependencies #normalization
 
   return(sum(
-      curr_capacity_monday,
-      curr_capacity_tuesday,
-      curr_capacity_wednesday,
-      curr_capacity_thursday,
-      curr_capacity_friday,
-      curr_violations_consecutiveDays_all,
-      curr_violations_consecutiveDays_none,
-      curr_violations_dependencies_monday,
-      curr_violations_dependencies_tuesday,
-      curr_violations_dependencies_wednesday,
-      curr_violations_dependencies_thursday,
-      curr_violations_dependencies_friday
+      currCapacityMonday,
+      currCapacityTuesday,
+      currCapacityWednesday,
+      currCapacityThursday,
+      currCapacityFriday,
+      currViolationsConsecutiveDaysAll,
+      currViolationsConsecutiveDaysNone,
+      currViolationsDependenciesMonday	,
+      currViolationsDependenciesTuesday,
+      currViolationsDependenciesWednesday,
+      currViolationsDependenciesThursday,
+      currViolationsDependenciesFriday
       ))
 }
 
