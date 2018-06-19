f854b0f62d912f8e603511f6386e12bf065650c1
Sérgio Giraldo <sergiorgiraldo@gmail.com>
2018-06-19 16:54:51 -0300

    20180619

--- disponibilidadeTransacoesJun05-06-07.R
+++ disponibilidadeTransacoesJun05-06-07.R
@@ -1,12 +1,20 @@
 library(ggplot2)
 library(stringr)
+library(lubridate)
 
 trans <- read.csv("C:\\Users\\sgiraldo\\source\\R\\transacoesJun05-06-07.csv")
 
-trans$date <- str_split_fixed(trans$timestamp, "_", Inf)[,1]
-trans$minute <- str_split_fixed(trans$timestamp, "_", Inf)[,2]
-trans$timestamp <- NULL
-trans <- trans[trans$date == "20180605",]
+trans$time <- as.numeric(str_split_fixed(trans$time, ":", Inf)[,1]) * 60 + as.numeric(str_split_fixed(trans$time, ":", Inf)[,2])
 
-plot(trans, x = trans$minutes, y = trans$total)
+# trans$date <- str_split_fixed(trans$timestamp, "_", Inf)[,1] 
+# trans$minute <- str_split_fixed(trans$timestamp, "_", Inf)[,2]
+# trans$timestamp <- NULL
+# trans <- trans[trans$date == "20180605",]
+
+ggplot(data=trans, aes(x=trans$time, y=trans$qtyTrans, group=1)) +
+  geom_line()+
+  geom_point()
+
+model <- lm(trans$qtyTrans ~ poly(trans$time, 3))
+summary(model)
 
