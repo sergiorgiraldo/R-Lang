87d879a36d83584aa8fe205cd85b6756dcbee576
sergio giraldo <sergiorgiraldo@gmail.com>
2018-08-28 21:42:41 -0300

    20180828

--- tapStats/sales.R
+++ tapStats/sales.R
@@ -1,4 +1,4 @@
-data <- read.csv("c:/users/sgiraldo/src/R-lang/tapStats/sales.csv")
+data <- read.csv("c:/Users/Sergio/source/R-Lang/tapStats/sales.csv")
 data.lm <- lm(sales ~ clients, data=data)
 
 plot(data$sales ~ data$clients, pch=15)
